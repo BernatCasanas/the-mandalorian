669030729c39f7d11c3ea45533e638e556f535a2
Ap011y0n <47557365+Ap011y0n@users.noreply.github.com>
2021-03-05 19:20:20 +0100

    [CODE] respawn

--- Diamond Engine/Source Code/CO_Collider.cpp
+++ Diamond Engine/Source Code/CO_Collider.cpp
@@ -144,6 +144,7 @@ void C_Collider::Update()
 
 		float3 half_size = { boxCollider.halfExtents.x, boxCollider.halfExtents.y, boxCollider.halfExtents.z };
 		
+		
 		glPushMatrix();
 		glMultMatrixf(trans.Transposed().ptr());
 
