58aa09d17d02ae41b4f519e9f3b13b9fa533d55d
Marc Pagès Francesch <47534017+marcpages2020@users.noreply.github.com>
2021-03-06 17:33:47 +0100

    [CODE] Taking name from material

--- Diamond Engine/Source Code/CO_Material.cpp
+++ Diamond Engine/Source Code/CO_Material.cpp
@@ -97,6 +97,9 @@ bool C_Material::OnEditor()
 		//	}
 		//}
 
+		ImGui::Text("Current material: ");
+		ImGui::SameLine();
+		ImGui::TextColored(ImVec4(.2, .2, .8, 1.0), "%s", material->name);
 		ImGui::Text("Drop here to change material");
 		if (ImGui::BeginDragDropTarget())
 		{
