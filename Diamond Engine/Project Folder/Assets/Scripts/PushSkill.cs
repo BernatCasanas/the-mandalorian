74551bed77436aa0bb23e28820132fb342f8982f
jose-tello <47975174+jose-tello@users.noreply.github.com>
2021-03-19 23:12:56 +0100

    [SCRIPTING] added grogu skill debug visual support, skill moves by script

--- Diamond Engine/Project Folder/Assets/Scripts/PushSkill.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/PushSkill.cs
@@ -3,9 +3,21 @@ using DiamondEngine;
 
 public class PushSkill : DiamondComponent
 {
+	public float speed = 60.0f;
+	public float maxLifeTime = 5.0f;
+
+	private float currentLifeTime = 0.0f;
+
 	public void Update()
 	{
+		currentLifeTime += Time.deltaTime;
 
+		gameObject.transform.localPosition += gameObject.transform.GetForward() * (speed * Time.deltaTime);
+
+		if (currentLifeTime >= maxLifeTime)
+		{
+			InternalCalls.Destroy(this.gameObject);
+		}
 	}
 
 }
