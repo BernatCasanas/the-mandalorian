fb0ea1ba7341a2180936c2ef9cd8a2ccc33c2bdd
pgalmor <47526823+PoleteTomaPedete@users.noreply.github.com>
2021-03-22 01:18:51 +0100

    [SCENE] Cutscene's text updated

--- Diamond Engine/Project Folder/Assets/Scripts/TextController.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/TextController.cs
@@ -18,12 +18,12 @@ public class TextController : DiamondComponent
 	{
 		"Well kid, we should get going if we want to kill that     Rancor before nightfall. You know this desert can get  really dangerous at night, with all those Tusken           Raiders creeping around.",
 		"(Looks at him, without any light of understandment      shining in his eyes)",
-		"You don’t remember? We agreed with that woman thatif we defeated the Rancor that escaped from Jabba’s    palace she would help us find the Jedi.",
-		"We need her if we want to finally find the Moff Gideonand put an end to this story, I don’t want you to be in   danger all the time.",
+		"You don't remember? We agreed with that woman     that if we defeated the Rancor that escaped from        Jabba's palace she would help us find the Jedi.",
+		"We need her if we want to finally find the Moff Gideonand put an end to this story, I don't want you to be in  danger all the time.",
 		"(Closes his eyes, as if just hearing that name scared    him)",
-		"It’s ok kid, don’t worry, he’s not here now, he cannot     harm you. But we have to defeat him so he leaves us   alone, you know he won’t rest until we stop him.",
+		"It's ok kid, don't worry, he's not here now, he cannot  harm you. But we have to defeat him so he leaves us   alone, you know he won't rest until we stop him.",
 		"(Slowly calms and opens his eyes, still scared, and     looks at MANDO)",
-		"Let’s go now, this zone of Tatooine has been controlledby the Empire since Jabba fell, and even now that       it’s gone I wouldn’t expect a pleasant ride.",
+		"Let's go now, this zone of Tatooine has been              controlled by the Empire since Jabba fell, and even     now that it's gone I wouldn't expect a pleasant ride.",
 
 	};
 	private List<string> images = new List<string>()
