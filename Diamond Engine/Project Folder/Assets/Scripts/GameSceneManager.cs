9d50f899b4397e805db9eb7a92261f82aba7b9df
Ferran-Roger Basart i Bosch <47557484+ferba93@users.noreply.github.com>
2021-04-01 00:27:24 +0200

    [CODE] Scene manager module name fixed
    This avoids C# doing kaboom

--- Diamond Engine/Project Folder/Assets/Scripts/GameSceneManager.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/GameSceneManager.cs
@@ -1,14 +1,14 @@
 using System;
 using DiamondEngine;
 
-public class SceneManager : DiamondComponent
+public class GameSceneManager : DiamondComponent
 {
     bool enemiesHaveSpawned = false;
     EndLevelRewards rewardMenu = new EndLevelRewards();
 
     // THIS IS A VERY INCOMPLETE MODULE THAT IS CURRENTLY NOT MANAGING SCENE. HOWEVER, WE HAVE TO DO THAT IN A CLOSED PLACED. AND SOMEONE HAS TO START IT. YEET TIME :3
     public void Update()
-	{
+    {
 
         if (Counter.roomEnemies > 0)    // This would, ideally, not be necessary, and enemies generating would have nothing to do with dialogue ocurring, since it would (presumably) stop the game
         {
