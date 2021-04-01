f270b25f54c14c48e6b77eff234c893c0b706089
AlexMelenchon <melenchonatona@gmail.com>
2021-04-01 18:19:31 +0200

    [CODE] UID to CSGO

--- Diamond Engine/Source Code/MO_MonoManager.cpp
+++ Diamond Engine/Source Code/MO_MonoManager.cpp
@@ -189,6 +189,7 @@ bool M_MonoManager::Init()
 	mono_add_internal_call("DiamondEngine.GameObject::SetVelocity", SetVelocity);
 	mono_add_internal_call("DiamondEngine.GameObject::AddForce", AddForce);
 	mono_add_internal_call("DiamondEngine.GameObject::SetParent", CS_SetParent);
+	mono_add_internal_call("DiamondEngine.GameObject::GetUid", GetUid);
 
 	mono_add_internal_call("DiamondEngine.Animator::Play", Play);
 	mono_add_internal_call("DiamondEngine.Animator::Pause", Pause);
