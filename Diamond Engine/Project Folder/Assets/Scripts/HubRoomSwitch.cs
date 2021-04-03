8485e7543e54b4742c7dcfe0eba098bbe0a8ce64
pablogalve <pablogalve100@gmail.com>
2021-04-03 20:59:08 +0200

    [CODE] Text displayed: "Press A to start the run"

--- Diamond Engine/Project Folder/Assets/Scripts/HubRoomSwitch.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/HubRoomSwitch.cs
@@ -1,49 +1,19 @@
 using System;
+using System.Collections.Generic;
 using DiamondEngine;
 
 public class HubRoomSwitch : DiamondComponent
 {
-    public int nextRoomUID;
-    public GameObject colliderPosition;
-    public GameObject player;
-    public float maxDistance = 5;
-    public bool insideCollider = false;
-    
-    public bool IsInside()
-    {         
-        Vector3 playerPos = player.transform.globalPosition;
-        Vector3 colliderPos = colliderPosition.transform.globalPosition;
-        double insideNum = Math.Pow(playerPos.x - colliderPos.x, 2) + Math.Pow(playerPos.y - colliderPos.y, 2) + Math.Pow(playerPos.z - colliderPos.z, 2);
-        double distance = Math.Sqrt(insideNum);
-        Debug.Log("distance:" + distance);
-        Debug.Log("maxdistance:" + maxDistance);
-        if (distance >= -maxDistance && distance <= maxDistance)
-        {
-            Debug.Log("true");
-            return true;
-        }
+	public int nextRoomUID = -1;
 
-        else {
-            Debug.Log("false");
-            return false; }
-    }
+	public void OnExecuteButton()
+	{
+		SceneManager.LoadScene(nextRoomUID);
+	}
+	public void Update()
+	{
 
-    public void Update()
-    {
-        //Debug.Log("Update");
-        if (IsInside() == true)
-        {
-            //Show text
-            Debug.Log("Inside Collider");
-            if (Input.GetGamepadButton(DEControllerButton.A) == KeyState.KEY_DOWN || Input.GetGamepadButton(DEControllerButton.A) == KeyState.KEY_REPEAT)
-            {
-                Debug.Log("Key pressed");
-                SceneManager.LoadScene(nextRoomUID);
-            }
-        }
-        //else
-        //{
-        //    Debug.Log("Outside");
-        //}
-    }
+
+	}
+
 }
