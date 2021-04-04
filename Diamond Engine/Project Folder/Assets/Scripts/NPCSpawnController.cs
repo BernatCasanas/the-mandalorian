ae23489580764f1f76a0c1edfa834278c0abb781
pgalmor <pgalmor@users.noreply.github.com>
2021-04-04 19:17:06 +0200

    [HUB] NPCs load with correct animations and correct position

--- Diamond Engine/Project Folder/Assets/Scripts/NPCSpawnController.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/NPCSpawnController.cs
@@ -18,20 +18,29 @@ public class NPCSpawnController : DiamondComponent
     private List<int> charactersUID = new List<int>();
 
     public GameObject spawnPoint1 = null;
+    public int animation1 = 0;
     public GameObject spawnPoint2 = null;
+    public int animation2 = 0;
     public GameObject spawnPoint3 = null;
+    public int animation3 = 0;
     public GameObject spawnPoint4 = null;
+    public int animation4 = 0;
     public GameObject spawnPoint5 = null;
+    public int animation5 = 0;
     public GameObject spawnPoint6 = null;
+    public int animation6 = 0;
     public GameObject spawnPoint7 = null;
+    public int animation7 = 0;
     public GameObject spawnPoint8 = null;
+    public int animation8 = 0;
+
     private List<Tuple<Vector3, Quaternion, int>> spawnPoints = new List<Tuple<Vector3, Quaternion, int>>();
  
     enum State
     {
         IDLE = 0,
         SIT,
-        BAR
+        LEAN
     }
 
     bool start = true;
@@ -74,7 +83,7 @@ public class NPCSpawnController : DiamondComponent
             alreadyAppeared[randomIndex]++;
 
             //We update the coordinates of the already existing character
-            
+            Debug.Log(spawnPoints[randomIndex].Item3.ToString());
             SpawnUnit(charactersUID[i], GetCoordinatesFromSpawnPoint(randomIndex), GetRotationFromSpawnPoint(randomIndex), spawnPoints[randomIndex].Item3);
         }
     }
@@ -86,28 +95,28 @@ public class NPCSpawnController : DiamondComponent
 
         GameObject unit = InternalCalls.CreatePrefab(prefabPath, pos, rot, Vector3.one);
         //Animator animatorComponent = unit.GetComponent<Animator>();
-
+        Debug.Log("Character: " + prefabUID + " , Animation: " + animation);
         switch(prefabUID)
         {
             case GroguUID:
-                if(animation == 1) Animator.Play(unit, "Idle");
-                else if(animation == 2) Animator.Play(unit, "Sit");
-                else if(animation == 3) Animator.Play(unit, "Bar");
+                if(animation == 0) Animator.Play(unit, "Idle");
+                else if(animation == 1) Animator.Play(unit, "Sit");
+                else if(animation == 2) Animator.Play(unit, "Bar");
                 break;
             case BoKatanUID:
-                if (animation == 1) Animator.Play(unit, "Idle");
-                else if (animation == 2) Animator.Play(unit, "Sit");
-                else if (animation == 3) Animator.Play(unit, "Bar");
+                if (animation == 0) Animator.Play(unit, "BoKatan_Idle");
+                else if (animation == 1) Animator.Play(unit, "BoKatan_Sit");
+                else if (animation == 2) Animator.Play(unit, "BoKatan_Lean");
                 break;
             case AhsokaUID:
-                if (animation == 1) Animator.Play(unit, "Idle");
-                else if (animation == 2) Animator.Play(unit, "Sit");
-                else if (animation == 3) Animator.Play(unit, "Bar");
+                if (animation == 0) Animator.Play(unit, "Ahsoka_Idle");
+                else if (animation == 1) Animator.Play(unit, "Ahsoka_Sit");
+                else if (animation == 2) Animator.Play(unit, "Ahsoka_Lean");
                 break;
             case CaraDuneUID:
-                if (animation == 1) Animator.Play(unit, "Idle");
-                else if (animation == 2) Animator.Play(unit, "Sit");
-                else if (animation == 3) Animator.Play(unit, "Bar");
+                if (animation == 0) Animator.Play(unit, "Idle");
+                else if (animation == 1) Animator.Play(unit, "Sit");
+                else if (animation == 2) Animator.Play(unit, "Bar");
                 break;
             default:
                 break;
@@ -118,7 +127,7 @@ public class NPCSpawnController : DiamondComponent
     {
         if (spawnPoints[index] != null)
             return spawnPoints[index].Item1;
-        
+        Debug.Log("texto");
         Vector3 defaultVal = new Vector3(0,1,0);
         return defaultVal;
     }
@@ -137,71 +146,62 @@ public class NPCSpawnController : DiamondComponent
         //Load spawnPoints position into a List or other data structure to handle information more efficiently
         Vector3 positionAux = new Vector3(1,1,1);
         Quaternion rotationAux = new Quaternion(0,0,0,1);
-        int stateAux = (int)State.IDLE;
         Tuple<Vector3, Quaternion, int> TupleAux = new Tuple<Vector3, Quaternion, int>(positionAux, rotationAux, 1);
 
         if (spawnPoint1 != null)
         {
             positionAux = spawnPoint1.transform.globalPosition;
             rotationAux = spawnPoint1.transform.globalRotation;
-            stateAux = (int)State.IDLE;
-            TupleAux = Tuple.Create(positionAux, rotationAux, stateAux);
+            TupleAux = Tuple.Create(positionAux, rotationAux, animation1);
             spawnPoints.Add(TupleAux); 
         }
         if (spawnPoint2 != null)
         {
             positionAux = spawnPoint2.transform.globalPosition;
             rotationAux = spawnPoint2.transform.globalRotation;
-            stateAux = (int)State.IDLE;
-            TupleAux = Tuple.Create(positionAux, rotationAux, stateAux);
+            TupleAux = Tuple.Create(positionAux, rotationAux, animation2);
             spawnPoints.Add(TupleAux);
         }
         if (spawnPoint3 != null)
         {
             positionAux = spawnPoint3.transform.globalPosition;
             rotationAux = spawnPoint3.transform.globalRotation;
-            stateAux = (int)State.IDLE;
-            TupleAux = Tuple.Create(positionAux, rotationAux, stateAux);
+            TupleAux = Tuple.Create(positionAux, rotationAux, animation3);
             spawnPoints.Add(TupleAux);
         }
         if (spawnPoint4 != null)
         {
             positionAux = spawnPoint4.transform.globalPosition;
             rotationAux = spawnPoint4.transform.globalRotation;
-            stateAux = (int)State.IDLE;
-            TupleAux = Tuple.Create(positionAux, rotationAux, stateAux);
+            TupleAux = Tuple.Create(positionAux, rotationAux, animation4);
             spawnPoints.Add(TupleAux);
         }
         if (spawnPoint5 != null)
         {
             positionAux = spawnPoint5.transform.globalPosition;
             rotationAux = spawnPoint5.transform.globalRotation;
-            stateAux = (int)State.IDLE;
-            TupleAux = Tuple.Create(positionAux, rotationAux, stateAux);
+            TupleAux = Tuple.Create(positionAux, rotationAux, animation5);
             spawnPoints.Add(TupleAux);
         }
         if (spawnPoint6 != null)
         {
             positionAux = spawnPoint6.transform.globalPosition;
             rotationAux = spawnPoint6.transform.globalRotation;
-            stateAux = (int)State.IDLE;
-            TupleAux = Tuple.Create(positionAux, rotationAux, stateAux);
+            TupleAux = Tuple.Create(positionAux, rotationAux, animation6);
             spawnPoints.Add(TupleAux);
         }
         if (spawnPoint7 != null)
         {
             positionAux = spawnPoint7.transform.globalPosition;
             rotationAux = spawnPoint7.transform.globalRotation;
-            stateAux = (int)State.IDLE;
-            TupleAux = Tuple.Create(positionAux, rotationAux, stateAux);
+            TupleAux = Tuple.Create(positionAux, rotationAux, animation7);
             spawnPoints.Add(TupleAux);
         }
         if (spawnPoint8 != null)
         {
             positionAux = spawnPoint8.transform.globalPosition;
             rotationAux = spawnPoint8.transform.globalRotation;
-            stateAux = (int)State.IDLE;
-            TupleAux = Tuple.Create(positionAux, rotationAux, stateAux);
+            TupleAux = Tuple.Create(positionAux, rotationAux, animation8);
             spawnPoints.Add(TupleAux);
         }
     }
