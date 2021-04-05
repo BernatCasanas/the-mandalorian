a725906d84b9f4bc354134fb7dfb1bd847bd9adc
Oscar Royo <oscarroyo4@gmail.com>
2021-04-05 19:26:27 +0200

    shop script bugged?? not changing ingame

--- Diamond Engine/Project Folder/Assets/Scripts/SHOP.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/SHOP.cs
@@ -27,19 +27,39 @@ public class SHOP : DiamondComponent
         return Mathf.Distance(gameObject.transform.globalPosition, point) < givenRange;
     }
 
-    public void Buy(int item, int currency)
+    public void Buy(int item)
     {
         int cost = -1;
-
+        int currency = hud.GetComponent<HUD>().currency;
         switch (item)
         {
             case 0:
+                if(currency > 150)
+                {
+                    Debug.Log("Bought Item 1");
+                    cost = currency - 150;
+                }
                 break;
             case 1:
+                if (currency > 200)
+                {
+                    Debug.Log("Bought Item 2");
+                    cost = currency - 200;
+                }
                 break;
             case 2:
+                if (currency > 250)
+                {
+                    Debug.Log("Bought Item 3");
+                    cost = currency - 250;
+                }
                 break;
             case 3:
+                if (currency > 75)
+                {
+                    Debug.Log("Bought Health");
+                    cost = currency - 75;
+                }
                 break;
         }
 
@@ -50,6 +70,30 @@ public class SHOP : DiamondComponent
             hud.GetComponent<HUD>().UpdateCurrency(currency);
         }
 
+    }
+
+    public void OnExecuteButton()
+    {
+        if (gameObject.Name == "Button1")
+        {
+            Buy(0);
+        }
+        else if (gameObject.Name == "Button2")
+        {
+            Buy(1);
+        }
+        else if (gameObject.Name == "Button3")
+        {
+            Buy(2);
+        }
+        else if (gameObject.Name == "ButtonHealth")
+        {
+            Buy(3);
+        }
+        else if (gameObject.Name == "ButtonBack")
+        {
+            shopUI.Enable(!shopUI.IsEnabled());
+        }
     }
 
 }
