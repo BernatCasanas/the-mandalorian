7a4ee7fa2c55733aacc382619403b49b6acf1d19
Arnau77 <Arnau7711@hotmail.com>
2021-04-05 16:37:12 +0200

    start function didn't work, so again use the variable start instead of the function

--- Diamond Engine/Project Folder/Assets/Scripts/HubTextController.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/HubTextController.cs
@@ -46,18 +46,19 @@ public class HubTextController : DiamondComponent
 
 	private int total_interactions_and_stages = 0;
 	private bool dialog_finished = false;
+	private bool start = true;
 
 	
 
 	Interaction interaction = Interaction.NONE;
 
-	public void Start()
-    {
-		total_interactions_and_stages = total_stages * total_interactions;
-	}
 	public void Update()
 	{
-
+        if (start)
+        {
+			total_interactions_and_stages = total_stages * total_interactions;
+			start = false;
+        }
 		if (mando == null || Input.GetGamepadButton(DEControllerButton.A) != KeyState.KEY_DOWN || textController == null || dialog == null || textController.IsEnabled() == false) 
 			return;
         if (dialog_finished)
