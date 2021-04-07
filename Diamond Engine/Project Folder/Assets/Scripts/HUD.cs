86b347d97fd42163099bf0ee35a18a8f16a8a7db
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2021-04-07 10:53:50 +0200

    [HUD] Hud update in Room 3 and prefab

--- Diamond Engine/Project Folder/Assets/Scripts/HUD.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/HUD.cs
@@ -75,6 +75,7 @@ public class HUD : DiamondComponent
     public GameObject force_wave_second = null;
     public GameObject force_wave_third = null;
     public GameObject hp_glitch = null;
+    public GameObject max_hp_number = null;
     private bool start = true;
     private float pulsation_rate = 0.0f;
     private bool pulsation_forward = true;
@@ -118,6 +119,8 @@ public class HUD : DiamondComponent
             UpdateHP(PlayerHealth.currHealth, PlayerHealth.currMaxHealth);
             ResetCombo();
             UpdateForce(force, max_force);
+            max_hp_number.GetComponent<Text>().text = PlayerHealth.currMaxHealth.ToString();
+            last_hp = Mathf.Lerp(last_hp, PlayerHealth.currHealth - 0.5f, 2.5f * Time.deltaTime);
             force_bar_rate = -0.15f;
             start = false;
         }
@@ -274,7 +277,7 @@ public class HUD : DiamondComponent
             hp_bar.GetComponent<Material>().SetFloatUniform("t", pulsation_rate);
 
         }
-        //hp_glitch.Enable(false);
+        hp_glitch.Enable(false);
         if (hp_descending && hp_glitch != null)
         {
             hp_glitch.Enable(true);
@@ -460,20 +463,21 @@ public class HUD : DiamondComponent
             return;
         hp_bar.GetComponent<Material>().SetFloatUniform("length_used", hp_float);
         hp_bar.GetComponent<Material>().SetFloatUniform("last_hp", last_hp / max_hp);
-        //hp_glitch.GetComponent<Material>().SetFloatUniform("length_used", hp_float);
+        hp_glitch.GetComponent<Material>().SetFloatUniform("length_used", hp_float);
+        max_hp_number.GetComponent<Text>().text = PlayerHealth.currMaxHealth.ToString();
     }
 
     public void UpdateForce(int new_force, int max_force)
     {
-        if (force_bar == null || force_wave == null /*|| force_wave_second == null || force_wave_third == null*/)
+        if (force_bar == null || force_wave == null || force_wave_second == null || force_wave_third == null)
             return;
         float force_float = new_force;
         force_float /= max_force;
         //force_float = Math.Max(force_float, 1.0f); CANNOT DO THIS! This returns 1.0 and it's not a wanted value
         force_bar.GetComponent<Material>().SetFloatUniform("length_used", force_float);
         force_wave.GetComponent<Material>().SetFloatUniform("length_used", force_float);
-        //force_wave_second.GetComponent<Material>().SetFloatUniform("length_used", force_float);
-        //force_wave_third.GetComponent<Material>().SetFloatUniform("length_used", force_float);
+        force_wave_second.GetComponent<Material>().SetFloatUniform("length_used", force_float);
+        force_wave_third.GetComponent<Material>().SetFloatUniform("length_used", force_float);
     }
 
     public void ChangeAlphaSkillPush(bool alpha_full)
