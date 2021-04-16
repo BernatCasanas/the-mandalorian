4bd95109f36708dec4090fd7cd15ce181bbfbbdd
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2021-04-16 19:46:15 +0200

    [CODE] Now debug menu pauses-unpauses the game + updated prefab

--- Diamond Engine/Project Folder/Assets/Scripts/DebugReturn.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/DebugReturn.cs
@@ -4,11 +4,24 @@ using DiamondEngine;
 public class DebugReturn : DiamondComponent
 {
     public GameObject window = null;
+    private bool start = true;
 
     public void OnExecuteButton()
     {
+        Debug.Log("Deleting debug menu");
+        Time.ResumeGame();
+
         if (window != null)
             InternalCalls.Destroy(window);
+    }
+
+    public void Update()
+    {
+        if (start)
+        {
+            Time.PauseGame();
+            start = false;
+        }
     }
 
 }
