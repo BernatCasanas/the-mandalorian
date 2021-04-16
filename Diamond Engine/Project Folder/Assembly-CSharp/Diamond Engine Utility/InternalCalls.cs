e07f46529a49de288fd5821f70550d71e5d8adc5
jose-tello <47975174+jose-tello@users.noreply.github.com>
2021-04-16 23:54:16 +0200

    [SCRIPTING] Debug menu finished

--- Diamond Engine/Project Folder/Assembly-CSharp/Diamond Engine Utility/InternalCalls.cs
+++ Diamond Engine/Project Folder/Assembly-CSharp/Diamond Engine Utility/InternalCalls.cs
@@ -28,6 +28,9 @@ namespace DiamondEngine
         public static extern GameObject CreatePrefab(object prefab_path, object position, object rotation, object scale);
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        public static extern GameObject CreateUIPrefab(object prefab_path, object position, object rotation, object scale);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
         public static extern Vector3 GetWalkablePointAround(object position, float radius);
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
