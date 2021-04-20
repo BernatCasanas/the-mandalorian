4fd8d43189906736b809016c9bc67077da1d86b5
jose-tello <47975174+jose-tello@users.noreply.github.com>
2021-04-20 11:23:27 +0200

    cleaned particle system array buffer when done using it

--- Diamond Engine/Project Folder/Assets/Shaders/plane_vertex_colors.glsl
+++ Diamond Engine/Project Folder/Assets/Shaders/plane_vertex_colors.glsl
@@ -104,15 +104,15 @@ float ShadowCalculation(vec4 fragPosLightSpace, vec3 lightDir, vec3 normal)
 	float shadow = 0.0;
 	
     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
-    for(int x = -2; x <= 2; ++x)
+    for(int x = -1; x <= 1; ++x)
     {
-        for(int y = -2; y <= 2; ++y)
+        for(int y = -1; y <= 1; ++y)
         {
             float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
             shadow += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;        
         }    
     }
-    shadow /= 25.0;
+    shadow /= 9.0;
     
     // keep the shadow at 0.0 when outside the far_plane region of the light's frustum.
     if(projCoords.z > 1.0)
@@ -145,3 +145,4 @@ void main()
     color = vec4(lighting * fs_in.vertexColor, 1.0);
 }
 #endif
+
