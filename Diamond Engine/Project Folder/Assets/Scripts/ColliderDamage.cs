81487b59c5747f3dd5251c62b7a2de379b8e957f
Jordi Pardo <jordipardogutierrez@gmail.com>
2021-04-21 09:41:41 +0200

    [CODE] Added ColliderDamage.cs to Traps

---
+++ Diamond Engine/Project Folder/Assets/Scripts/ColliderDamage.cs
@@ -0,0 +1,16 @@
+﻿using DiamondEngine;
+
+class ColliderDamage : DiamondComponent
+{
+    public int damage;
+    public void OnTriggerEnter(GameObject triggeredGameObject)
+    {
+        if (triggeredGameObject.CompareTag("Player"))
+        {
+            PlayerHealth health = triggeredGameObject.GetComponent<PlayerHealth>();
+            if (health != null)
+                health.TakeDamage(damage);
+        }
+    }
+}
+
