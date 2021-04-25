ba6366a1c3de008fae435a13017c7e3c50da6333
Ferran-Roger Basart i Bosch <47557484+ferba93@users.noreply.github.com>
2021-04-25 20:01:06 +0200

    [Cinematic Intro] Cinematic for Alpha 1 final version

--- Diamond Engine/Project Folder/Assets/Scripts/IntroCinematic.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/IntroCinematic.cs
@@ -23,11 +23,11 @@ public class IntroCinematic : DiamondComponent
 
     GameObject[] pointArray = null;
     float[] speedArray = new float[] { 0.5f, 2.0f, 1.0f, 1.0f };    // Adapt values
-    int arrayCount = -1;
+    int arrayCount = 0; // To have the helmet part, this should be at -1
 
     public void Awake()
     {
-     /*   if (Counter.firstRun)
+        if (Counter.firstRun)
         {
             auxCameraRotation = cameraObject.transform.localRotation;
             pointArray = new GameObject[] { point1, point2, point3, point4, point5, point6, point7, point8 };
@@ -38,26 +38,23 @@ public class IntroCinematic : DiamondComponent
         else
         {
             EndCinematic();
-        }*/
+        }
     }
 
     public void Update()
     {
-     /*   if (toGoPosition != null)
+        // We should have a way to skip this :/
+        if (toGoPosition != null)
         {
-            //        Debug.Log("Camera position " + cameraAuxPosition);
-            //         Debug.Log("Go vector position " + toGoPosition);
-            //          Debug.Log("Array count " + arrayCount);
             cameraAuxPosition += (toGoPosition - cameraAuxPosition).normalized * Time.deltaTime * currentSpeed;
             cameraObject.transform.localRotation = Quaternion.Slerp(cameraObject.transform.localRotation, toRotateQuaternion, 0.25f * Time.deltaTime);
             cameraObject.transform.localPosition = cameraAuxPosition;
         }
 
-        //        Debug.Log("Distance is " + Mathf.Distance(cameraObject.transform.localPosition, toGoPosition).ToString());
         if (Mathf.Distance(cameraAuxPosition, toGoPosition) < 0.5f)
         {
             UpdateValues();
-        }*/
+        }
     }
 
     public void UpdateValues()
@@ -75,7 +72,6 @@ public class IntroCinematic : DiamondComponent
         cameraObject.transform.localRotation = pointArray[arrayCount * 2].transform.localRotation;
         toGoPosition = pointArray[(arrayCount * 2) + 1].transform.localPosition;
         toRotateQuaternion = pointArray[(arrayCount * 2) + 1].transform.localRotation;
-        //Debug.Log("Camera position is " + cameraObject.transform.localPosition);
     }
 
     public void EndCinematic()
