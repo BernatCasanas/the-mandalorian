f9064af41393309c9664f81c5af58a1cd703c065
carlosredolar <carlos.redolar.torres@gmail.com>
2021-04-30 21:38:46 +0200

    [CODE] deathtrooper behaviour finished

---
+++ Diamond Engine/Project Folder/Assets/Scripts/Deathtrooper.cs
@@ -0,0 +1,621 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+using DiamondEngine;
+
+public class Deathtrooper : Enemy
+{
+    enum STATE : int
+    {
+        NONE = -1,
+        IDLE,
+        WANDER,
+        RUN,
+        PUSHED,
+        SHOOT,
+        HIT,
+        DIE
+    }
+
+    enum INPUT : int
+    {
+        IN_IDLE,
+        IN_IDLE_END,
+        IN_RUN,
+        IN_RUN_END,
+        IN_WANDER,
+        IN_PUSHED,
+        IN_SHOOT,
+        IN_SHOOT_END,
+        IN_HIT,
+        IN_DIE,
+        IN_PLAYER_IN_RANGE,
+        IN_SHOT_RANGE
+    }
+
+    //State
+    private STATE currentState = STATE.NONE;
+
+    private List<INPUT> inputsList = new List<INPUT>();
+
+    public GameObject shootPoint = null;
+    //public GameObject hitParticles = null;
+
+    //Action times
+    public float idleTime = 5.0f;
+    private float dieTime = 3.0f;
+    public float timeBewteenShots = 0.5f;
+    public float betweenStatesTime = 1.5f;
+    public float recoilTime = 0.0f;
+    public float shotCDTime = 0.0f;
+
+    //Speeds
+    public float wanderSpeed = 3.5f;
+    public float runningSpeed = 7.5f;
+    //public float bulletSpeed = 10.0f;
+    private bool skill_slowDownActive = false;
+    public float recoilSpeed = 0.0f;
+
+    //Ranges
+    public float wanderRange = 7.5f;
+    public float shotRange = 12.5f;
+
+    //Timers
+    private float idleTimer = 0.0f;
+    //private float shotTimer = 0.0f;
+    private float dieTimer = 0.0f;
+    private float shootTimer = 0.0f;
+    private float pushTimer = 0.0f;
+    private float skill_slowDownTimer = 0.0f;
+    private float recoilTimer = 0.0f;
+    private float betweenStatesTimer = 0.0f;
+    public float shotCDTimer = 0.0f;
+
+    //Action variables
+    private int shotsShooted = 0;
+    public int maxShots = 2;
+    private bool canShoot = true;
+
+
+    //push
+    public float pushHorizontalForce = 100;
+    public float pushVerticalForce = 10;
+    public float PushStun = 2;
+
+    public void Awake()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        targetPosition = null;
+
+        currentState = STATE.IDLE;
+        Animator.Play(gameObject, "ST_Idle");
+
+        idleTimer = idleTime;
+    }
+
+    public void Update()
+    {
+        #region STATE MACHINE
+
+        ProcessInternalInput();
+        ProcessExternalInput();
+        ProcessState();
+
+        UpdateState();
+
+        #endregion
+    }
+
+
+    //Timers go here
+    private void ProcessInternalInput()
+    {
+        if (idleTimer > 0.0f)
+        {
+            idleTimer -= Time.deltaTime;
+
+            if (idleTimer <= 0.0f)
+            {
+                inputsList.Add(INPUT.IN_WANDER);
+            }
+        }
+
+        if (currentState == STATE.RUN || currentState == STATE.WANDER)
+        {
+            if (Mathf.Distance(gameObject.transform.globalPosition, agent.GetDestination()) <= agent.stoppingDistance)
+            {
+                inputsList.Add(INPUT.IN_IDLE);
+            }
+        }
+
+        if(shotCDTimer > 0.0f)
+        {
+            shotCDTimer -= Time.deltaTime;
+            if(shotCDTimer <= 0.0f)
+            {
+                canShoot = true;
+            }
+        }
+
+        if (skill_slowDownActive)
+        {
+            skill_slowDownTimer += Time.deltaTime;
+            if (skill_slowDownTimer >= Skill_Tree_Data.GetWeaponsSkillTree().PW3_SlowDownDuration)
+            {
+                skill_slowDownTimer = 0.0f;
+                skill_slowDownActive = false;
+            }
+        }
+    }
+
+    //All events from outside the stormtrooper
+    private void ProcessExternalInput()
+    {
+        if (currentState != STATE.DIE)
+        {
+            if (Core.instance.gameObject == null)
+                return;
+
+            if (InRange(Core.instance.gameObject.transform.globalPosition, detectionRange))
+            {
+                inputsList.Add(INPUT.IN_PLAYER_IN_RANGE);
+                //LookAt(Core.instance.gameObject.transform.globalPosition);
+            }
+            if (InRange(Core.instance.gameObject.transform.globalPosition, shotRange) && canShoot)
+            {
+                inputsList.Add(INPUT.IN_SHOT_RANGE);
+                //LookAt(Core.instance.gameObject.transform.globalPosition);
+            }
+        }
+    }
+
+    //Manages state changes throught inputs
+    private void ProcessState()
+    {
+        while (inputsList.Count > 0)
+        {
+            INPUT input = inputsList[0];
+
+            switch (currentState)
+            {
+                case STATE.NONE:
+                    Debug.Log("CORE ERROR STATE");
+                    break;
+
+                case STATE.IDLE:
+                    switch (input)
+                    {
+                        case INPUT.IN_WANDER:
+                            currentState = STATE.WANDER;
+                            StartWander();
+                            break;
+
+                        case INPUT.IN_PLAYER_IN_RANGE:
+                            currentState = STATE.RUN;
+                            PlayerDetected();
+                            StartRun();
+                            break;
+
+                        case INPUT.IN_PUSHED:
+                            currentState = STATE.PUSHED;
+                            StartPush();
+                            break;
+
+                        case INPUT.IN_DIE:
+                            currentState = STATE.DIE;
+                            StartDie();
+                            break;
+                    }
+                    break;
+
+                case STATE.WANDER:
+                    switch (input)
+                    {
+                        case INPUT.IN_IDLE:
+                            currentState = STATE.IDLE;
+                            WanderEnd();
+                            StartIdle();
+                            break;
+
+                        case INPUT.IN_PLAYER_IN_RANGE:
+                            currentState = STATE.RUN;
+                            WanderEnd();
+                            PlayerDetected();
+                            StartRun();
+                            break;
+
+                        case INPUT.IN_PUSHED:
+                            currentState = STATE.PUSHED;
+                            StartPush();
+                            break;
+
+                        case INPUT.IN_DIE:
+                            currentState = STATE.DIE;
+                            WanderEnd();
+                            StartDie();
+                            break;
+                    }
+                    break;
+
+                case STATE.RUN:
+                    switch (input)
+                    {
+                        case INPUT.IN_RUN_END:
+                            currentState = STATE.IDLE;
+                            RunEnd();
+                            StartIdle();
+                            break;
+
+                        case INPUT.IN_SHOT_RANGE:
+                            currentState = STATE.SHOOT;
+                            RunEnd();
+                            StartShoot();
+                            break;
+
+                        case INPUT.IN_PUSHED:
+                            currentState = STATE.PUSHED;
+                            RunEnd();
+                            StartPush();
+                            break;
+
+                        case INPUT.IN_DIE:
+                            currentState = STATE.DIE;
+                            RunEnd();
+                            StartDie();
+                            break;
+     
+                    }
+                    break;
+
+                case STATE.SHOOT:
+                    switch (input)
+                    {
+                        case INPUT.IN_SHOOT_END:
+                            currentState = STATE.RUN;
+                            StartRun();
+                            break;
+
+                        //case INPUT.IN_RUN:
+                        //    currentState = STATE.RUN;
+                        //    StartRun();
+                        //    break;
+
+                        case INPUT.IN_PUSHED:
+                            currentState = STATE.PUSHED;
+                            StartPush();
+                            break;
+
+                        case INPUT.IN_DIE:
+                            currentState = STATE.DIE;
+                            StartDie();
+                            break;
+                    }
+                    break;
+                case STATE.PUSHED:
+                    switch (input)
+                    {
+                        case INPUT.IN_IDLE:
+                            currentState = STATE.IDLE;
+                            StartIdle();
+                            break;
+
+                        case INPUT.IN_DIE:
+                            currentState = STATE.DIE;
+                            StartDie();
+                            break;
+                    }
+                    break;
+                default:
+                    Debug.Log("NEED TO ADD STATE TO CORE SWITCH");
+                    break;
+            }
+            inputsList.RemoveAt(0);
+        }
+    }
+
+
+    private void UpdateState()
+    {
+        switch (currentState)
+        {
+            case STATE.NONE:
+                break;
+            case STATE.IDLE:
+                break;
+            case STATE.RUN:
+                UpdateRun();
+                break;
+            case STATE.WANDER:
+                UpdateWander();
+                break;
+            case STATE.SHOOT:
+                UpdateShoot();
+                break;
+            case STATE.PUSHED:
+                UpdatePush();
+                break;
+            case STATE.DIE:
+                UpdateDie();
+                break;
+            default:
+                Debug.Log("NEED TO ADD STATE TO CORE");
+                break;
+        }
+    }
+
+    #region IDLE
+    private void StartIdle()
+    {
+        Debug.Log("DEATHTROOPER IDLE");
+        idleTimer = idleTime;
+        Animator.Play(gameObject, "ST_Idle");
+    }
+    #endregion
+
+    #region WANDER
+    private void StartWander()
+    {
+        Debug.Log("DEATHTROOPER WANDER");
+        agent.CalculateRandomPath(gameObject.transform.globalPosition, wanderRange);
+
+        Animator.Play(gameObject, "ST_Run");
+        Audio.PlayAudio(gameObject, "Play_Footsteps_Stormtrooper");
+    }
+    private void UpdateWander()
+    {
+        LookAt(agent.GetDestination());
+        if (skill_slowDownActive)
+            agent.MoveToCalculatedPos(wanderSpeed * (1 - Skill_Tree_Data.GetWeaponsSkillTree().PW3_SlowDownAmount));
+        else agent.MoveToCalculatedPos(wanderSpeed);
+    }
+    private void WanderEnd()
+    {
+        Audio.StopAudio(gameObject);
+    }
+    #endregion
+
+    #region RUN
+    private void StartRun()
+    {
+        Debug.Log("DEATHTROOPER RUN");
+        Animator.Play(gameObject, "ST_Run");
+    }
+    private void UpdateRun()
+    {
+        agent.CalculatePath(gameObject.transform.globalPosition, Core.instance.gameObject.transform.globalPosition);
+        LookAt(agent.GetDestination());
+        if (skill_slowDownActive)
+            agent.MoveToCalculatedPos(runningSpeed * (1 - Skill_Tree_Data.GetWeaponsSkillTree().PW3_SlowDownAmount));
+        else
+            agent.MoveToCalculatedPos(runningSpeed);
+    }
+    private void RunEnd()
+    {
+        Audio.StopAudio(gameObject);
+    }
+    #endregion
+
+    #region SHOOT
+    private void StartShoot()
+    {
+        Debug.Log("DEATHTROOPER SHOOT");
+        Animator.Play(gameObject, "ST_Shoot");
+        betweenStatesTimer = betweenStatesTime;
+    }
+
+    private void UpdateShoot()
+    {
+        if(betweenStatesTimer > 0.0f)
+        {
+            betweenStatesTimer -= Time.deltaTime;
+            LookAt(Core.instance.gameObject.transform.globalPosition);
+            if(betweenStatesTimer <= 0.0f)
+            {
+                Shoot();
+            }
+            
+        }
+
+        if (recoilTimer > 0.0f)
+        {
+            recoilTimer -= Time.deltaTime;
+            gameObject.transform.localPosition -= gameObject.transform.GetForward().normalized * recoilSpeed * Time.deltaTime;
+            if(recoilTimer <= 0.0f)
+            {
+                shotCDTimer = shotCDTime;
+                inputsList.Add(INPUT.IN_SHOOT_END);
+            }
+        }
+
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = InternalCalls.CreatePrefab("Library/Prefabs/1635392825.prefab", shootPoint.transform.globalPosition, shootPoint.transform.globalRotation, shootPoint.transform.globalScale);
+        bullet.GetComponent<BH_Bullet>().damage = damage;
+
+        Animator.Play(gameObject, "ST_Shoot");
+        Audio.PlayAudio(gameObject, "Play_Blaster_Stormtrooper");
+        //shotTimes++;
+        recoilTimer = recoilTime;
+        canShoot = false;
+    }
+    private void PlayerDetected()
+    {
+        //Debug.Log("SKYTROOPER PLAYER DETECTED");
+        Audio.PlayAudio(gameObject, "Play_Enemy_Detection");
+    }
+    #endregion
+
+    #region DIE
+    private void StartDie()
+    {
+        //Debug.Log("SKYTROOPER DIE");
+        dieTimer = dieTime;
+        //Audio.StopAudio(gameObject);
+
+        //Animator.Play(gameObject, "ST_Die", 1.0f);
+
+        Audio.PlayAudio(gameObject, "Play_Stormtrooper_Death");
+        Audio.PlayAudio(gameObject, "Play_Mando_Kill_Voice");
+
+        //Combo
+        if (PlayerResources.CheckBoon(BOONS.BOON_MASTERYODAASSITANCE))
+        {
+            Core.instance.hud.GetComponent<HUD>().AddToCombo(300, 1.0f);
+        }
+
+        RemoveFromEnemyList();
+    }
+    private void UpdateDie()
+    {
+        if (dieTimer > 0.0f)
+        {
+            dieTimer -= Time.deltaTime;
+
+            if (dieTimer <= 0.0f)
+            {
+                Die();
+            }
+        }
+    }
+
+    private void Die()
+    {
+        //float dist = (deathPoint.transform.globalPosition - gameObject.transform.globalPosition).magnitude;
+        Vector3 forward = gameObject.transform.GetForward();
+        //forward = forward.normalized * (-dist);
+        Counter.SumToCounterType(Counter.CounterTypes.ENEMY_STORMTROOP);
+        Counter.roomEnemies--;
+        Debug.Log("Enemies: " + Counter.roomEnemies.ToString());
+        if (Counter.roomEnemies <= 0)
+        {
+            Counter.allEnemiesDead = true;
+        }
+
+        //Created dropped coins
+        var rand = new Random();
+        int droppedCoins = rand.Next(1, 4);
+        for (int i = 0; i < droppedCoins; i++)
+        {
+            Vector3 pos = gameObject.transform.globalPosition;
+            pos.x += rand.Next(-200, 201) / 100;
+            pos.z += rand.Next(-200, 201) / 100;
+            InternalCalls.CreatePrefab(coinDropPath, pos, Quaternion.identity, new Vector3(0.07f, 0.07f, 0.07f));
+        }
+        Core.instance.gameObject.GetComponent<PlayerHealth>().TakeDamage(-PlayerHealth.healWhenKillingAnEnemy);
+        InternalCalls.CreatePrefab("Library/Prefabs/230945350.prefab", new Vector3(gameObject.transform.globalPosition.x + forward.x, gameObject.transform.globalPosition.y, gameObject.transform.globalPosition.z + forward.z), Quaternion.identity, new Vector3(1, 1, 1));
+        InternalCalls.Destroy(gameObject);
+    }
+
+    #endregion
+
+    #region PUSH
+
+    private void StartPush()
+    {
+        Vector3 force = gameObject.transform.globalPosition - Core.instance.gameObject.transform.globalPosition;
+        force.y = pushVerticalForce;
+        gameObject.AddForce(force * pushHorizontalForce);
+        pushTimer = 0.0f;
+    }
+    private void UpdatePush()
+    {
+        pushTimer += Time.deltaTime;
+        if (pushTimer >= PushStun)
+            inputsList.Add(INPUT.IN_IDLE);
+
+    }
+    #endregion
+
+    public void OnCollisionEnter(GameObject collidedGameObject)
+    {
+        if (collidedGameObject.CompareTag("Bullet"))
+        {
+            BH_Bullet bullet = collidedGameObject.GetComponent<BH_Bullet>();
+
+            if (bullet != null)
+            {
+                TakeDamage(bullet.damage);
+
+                Audio.PlayAudio(gameObject, "Play_Stormtrooper_Hit");
+
+                if (Core.instance.hud != null)
+                {
+                    HUD hudComponent = Core.instance.hud.GetComponent<HUD>();
+
+                    if (hudComponent != null)
+                        hudComponent.AddToCombo(25, 0.95f);
+                }
+
+                if (Skill_Tree_Data.IsEnabled((int)Skill_Tree_Data.SkillTreesNames.WEAPONS, (int)Skill_Tree_Data.WeaponsSkillNames.PRIMARY_SLOW_SPEED))
+                {
+                    skill_slowDownActive = true;
+                    skill_slowDownTimer = 0.0f;
+                }
+            }
+        }
+        else if (collidedGameObject.CompareTag("ChargeBullet"))
+        {
+            ChargedBullet bullet = collidedGameObject.GetComponent<ChargedBullet>();
+
+            if (bullet != null)
+            {
+                healthPoints -= bullet.damage;
+
+                TakeDamage(bullet.damage);
+
+                Audio.PlayAudio(gameObject, "Play_Stormtrooper_Hit");
+
+                if (Core.instance.hud != null)
+                {
+                    Core.instance.hud.GetComponent<HUD>().AddToCombo(55, 0.25f);
+                }
+
+                if (currentState != STATE.DIE && healthPoints <= 0.0f)
+                {
+                    inputsList.Add(INPUT.IN_DIE);
+                }
+            }
+        }
+        else if (collidedGameObject.CompareTag("ExplosiveBarrel") && collidedGameObject.GetComponent<SphereCollider>().active)
+        {
+            //if (myParticles != null && myParticles.hit != null)
+            //    myParticles.hit.Play();
+            BH_DestructBox explosion = collidedGameObject.GetComponent<BH_DestructBox>();
+
+            if (explosion != null)
+            {
+                healthPoints -= explosion.explosion_damage * 2;
+                if (currentState != STATE.DIE && healthPoints <= 0.0f)
+                    inputsList.Add(INPUT.IN_DIE);
+            }
+
+        }
+    }
+
+    public void OnTriggerEnter(GameObject triggeredGameObject)
+    {
+        if (triggeredGameObject.CompareTag("PushSkill") && currentState != STATE.PUSHED && currentState != STATE.DIE)
+        {
+            if (Core.instance.gameObject != null)
+            {
+                inputsList.Add(INPUT.IN_PUSHED);
+            }
+        }
+    }
+
+    public override void TakeDamage(float damage)
+    {
+        healthPoints -= damage;
+
+        if (currentState != STATE.DIE)
+        {
+            if (healthPoints <= 0.0f)
+                inputsList.Add(INPUT.IN_DIE);
+        }
+
+    }
+}
