40a57e87345d6780fcf8b758f5b8ad5e43c38c2d
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2021-05-03 13:09:39 +0200

    [CODE] Created DE_Advanced_FrameBuffer class

---
+++ Diamond Engine/Source Code/DE_Advanced_FrameBuffer.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include"MathGeoLib/include/Math/float2.h"
+#include"OpenGL.h"
+#include"Globals.h"
+
+enum class DEPTH_BUFFER_TYPE
+{
+	NONE,
+	DEPTH_TEXTURE,
+	DEPTH_RENDER_BUFER
+};
+
+class DE_Advanced_FrameBuffer
+{
+public:
+	//normal buffer constructor
+	DE_Advanced_FrameBuffer(int width,int height, DEPTH_BUFFER_TYPE depthBufferType);
+	//Multisampled buffer constructor
+	DE_Advanced_FrameBuffer(int width, int height,int msaaSamples);
+	~DE_Advanced_FrameBuffer();
+
+	void ClearBuffer();
+
+	void InitializeFrameBuffer(DEPTH_BUFFER_TYPE depthType);
+	void ReGenerateBuffer(int w, int h);
+
+	void BindFrameBuffer(int width, int height);
+	void UnbindFrameBuffer();
+	void BindToRead();
+
+	void ResolveToFBO(DE_Advanced_FrameBuffer& outputFbo);
+	void ResolveToScreen();
+
+	inline unsigned int GetFrameBuffer() { return framebuffer; }
+	inline unsigned int GetColorBuffer() { return colorBufferAttachment; }
+	inline unsigned int GetDepthBuffer() { return depthBufferAttachment; }
+
+	inline unsigned int GetColorTexture() { return colorTexture; }
+	inline unsigned int GetDepthTexture() { return depthTexture; }
+
+private:
+	int CreateFramBuffer();
+	void CreateMultisampleColorAttachment();
+	void CreateTextureAttachment();
+	void CreateDepthBufferAttachment();//TODO we do not support stencil for the moment
+	void CreateDepthTextureAttachment();//TODO we do not support stencil for the moment
+
+
+
+public:
+	float2 texBufferSize; //texture buffer dimensions
+private:
+
+	unsigned int framebuffer; //index of the frame buffer
+	unsigned int colorTexture; //index of the color texture
+	unsigned int depthTexture; //index of the depth texture
+	
+	unsigned int colorBufferAttachment; //index of the Color buffer
+	unsigned int depthBufferAttachment; //index of the Depth buffer
+
+	bool isMultisample;
+	int msaaSamples;
+	DEPTH_BUFFER_TYPE myDepthType;
+};
