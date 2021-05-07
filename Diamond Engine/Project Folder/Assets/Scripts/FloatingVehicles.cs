cd837a4c71cc80ed44297405a14b6735623768e7
Pablo Galve <pablogalve100@gmail.com>
2021-05-07 21:34:00 +0200

    [CODE] Type of vehicle detected from the script

--- Diamond Engine/Project Folder/Assets/Scripts/FloatingVehicles.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/FloatingVehicles.cs
@@ -3,45 +3,27 @@ using DiamondEngine;
 
 public class FloatingVehicles : DiamondComponent
 {
-	public float maxHeight = 1.0f;
-    public float randomPercentage = 0.1f;
-    public float rotSpeedDegSec = 1.0f;
-	public float verticalSpeedMultiplier = 0.5f;
-	bool goingUp = true;
-	float animTime = 0.0f;
+	private float maxHeight = 1.0f;
+	private float verticalSpeed = 0.5f;
+	private bool goingUp = true;
+	private float currPercentageOfAnimation = 0.0f;
     private Vector3 initialPos;
 
     public void Awake()
     {
         initialPos = gameObject.transform.localPosition;
-        maxHeight = GenerateRandomHeight();
+        SetVehicleProperties();
     }
 
     public void Update()
     {
-        if (goingUp)
-        {
-            animTime += Time.deltaTime * verticalSpeedMultiplier;
-        }
-        else
-        {
-            animTime -= Time.deltaTime * verticalSpeedMultiplier;
-        }
+        if (goingUp) currPercentageOfAnimation += Time.deltaTime * verticalSpeed;        
+        else currPercentageOfAnimation -= Time.deltaTime * verticalSpeed;        
 
-        if (animTime > maxHeight)
-        {
-            goingUp = false;
-            animTime = maxHeight;
-        }
-        else if (animTime < 0.0f)
-        {
-            goingUp = true;
-            animTime = 0.0f;
+        if (currPercentageOfAnimation > 1.0f) goingUp = false;
+        else if (currPercentageOfAnimation < 0.0f) goingUp = true;        
 
-        }
-
-        float yPos = ParametricBlend(animTime);
-
+        float yPos = ParametricBlend(currPercentageOfAnimation);
         Vector3 newPos = new Vector3(initialPos.x, initialPos.y, initialPos.z);
         newPos.y += yPos * maxHeight;
         gameObject.transform.localPosition = newPos;
@@ -49,12 +31,35 @@ public class FloatingVehicles : DiamondComponent
 
     public float ParametricBlend(float t) => ((t * t) / (2.0f * ((t * t) - t) + 1.0f));
 
-    private float GenerateRandomHeight()
+    /*private float GenerateRandomHeight()
     {
         float min = 1.0f - randomPercentage;
         float max = 1.0f + randomPercentage;
         System.Random random = new System.Random();
         double val = (random.NextDouble() * (max - min) + min);
         return (float)val;
+    }*/
+
+    private void SetVehicleProperties()
+    {
+        Debug.Log("-------------------GO Name:" + gameObject.Name);
+        switch (gameObject.Name)
+        {
+            case "LandSpeeder": 
+                maxHeight = 2.0f;
+                verticalSpeed = 0.3f;
+                break;
+            case "SpeederBike": 
+                maxHeight = 0.8f;
+                verticalSpeed = 0.5f;
+                break;
+            case "SnowSpeeder": 
+                maxHeight = 1.5f;
+                verticalSpeed = 0.15f;
+                break;
+            default:
+                Debug.Log("Vehicle properties have not been setup correctly");
+                break;            
+        }
     }
 }
