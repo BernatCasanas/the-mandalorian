d74cd3fb9b3f6211387de1bd161c5d51a9fcc14f
Eudalet <47557352+Hevne@users.noreply.github.com>
2021-05-09 16:04:49 +0200

    [CODE] Tutorial Buttons

--- Diamond Engine/Project Folder/Assets/Scripts/TutorialButtons.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/TutorialButtons.cs
@@ -5,16 +5,23 @@ public class TutorialButtons : DiamondComponent
 {
 	public GameObject dashPS = null;
 	public GameObject shootPS = null;
-	public GameObject groguPS = null;
+	public GameObject wallPS = null;
+	public GameObject pushPS = null;
+	public GameObject rifflePS = null;
+	public GameObject grenadePS = null;
+	public GameObject dashTrapPS = null;
+
 	
 	public GameObject dashXbox = null;
 	public GameObject shootXbox = null;
-	public GameObject groguXbox = null;
+	public GameObject wallXbox = null;
+	public GameObject pushXbox = null;
+	public GameObject riffleXbox = null;
+	public GameObject grenadeXbox = null;
+	public GameObject dashTrapXbox = null;
 
 	public void Awake()
 	{
-        if (Counter.firstRun == true)
-        {
 			ControllerType type = DiamondEngine.Input.GetControllerType();  // We call a function, we get enum / boolean results
 
             switch (type)
@@ -23,16 +30,23 @@ public class TutorialButtons : DiamondComponent
 				case ControllerType.SDL_CONTROLLER_TYPE_PS4:
 					dashPS.Enable(true);
 					shootPS.Enable(true);
-					groguPS.Enable(true);
+					wallPS.Enable(true);
+					pushPS.Enable(true);
+					rifflePS.Enable(true);
+					grenadePS.Enable(true);
+					dashTrapPS.Enable(true);
 					break;
 				case ControllerType.SDL_CONTROLLER_TYPE_XBOX360:
 				case ControllerType.SDL_CONTROLLER_TYPE_XBOXONE:
 					dashXbox.Enable(true);
 					shootXbox.Enable(true);
-					groguXbox.Enable(true);
+					wallXbox.Enable(true);
+					pushXbox.Enable(true);
+					riffleXbox.Enable(true);
+					grenadeXbox.Enable(true);
+					dashTrapXbox.Enable(true);
 					break;
             }
-		}
 	}
 
 }
