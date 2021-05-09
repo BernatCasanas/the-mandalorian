bac44a1207636e1444b9f06e3c29c968cd4faacc
Eudalet <47557352+Hevne@users.noreply.github.com>
2021-05-09 18:30:25 +0200

    [CODE] Button Loop

--- Diamond Engine/Project Folder/Assets/Scripts/TutoButtonLoop.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/TutoButtonLoop.cs
@@ -3,11 +3,38 @@ using DiamondEngine;
 
 public class TutoButtonLoop : DiamondComponent
 {
+	public int releasedButton = 0;
 	public int pressedButton = 0;
 	public int radiantButton = 0;
+	private int buttonToChange = 0;
+
+	Material buttonMaterial = null;
+	private float maxTime = 0.5f;
+	private float currentTime = 0.0f;
+	int loopState = 0;
+
 	public void Update()
 	{
+		//buttonMaterial=this.gameObject.GetComponent<Material>();
 
+		currentTime += Time.deltaTime;
+		if (currentTime >= maxTime)
+		{
+			switch (loopState)
+			{
+				case 0:
+					buttonToChange = pressedButton; loopState = 1;
+					break;
+				case 1:
+					buttonToChange = radiantButton; loopState = 2;
+					break;
+				case 2:
+					buttonToChange = releasedButton; loopState = 0;
+					break;
+			}
+			this.gameObject.AssignLibraryTextureToMaterial(buttonToChange, "diffuseTexture");
+			currentTime = 0.0f;
+		}
 	}
 
 }
