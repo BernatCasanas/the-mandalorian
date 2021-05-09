b20e7355240cd7707d7cafd184e7ad3230b10d4f
Eudalet <47557352+Hevne@users.noreply.github.com>
2021-05-09 17:32:30 +0200

    [CODE] Tutorial Buttons loop

---
+++ Diamond Engine/Project Folder/Assets/Scripts/TutoButtonLoop.cs
@@ -0,0 +1,13 @@
+using System;
+using DiamondEngine;
+
+public class TutoButtonLoop : DiamondComponent
+{
+	public int pressedButton = 0;
+	public int radiantButton = 0;
+	public void Update()
+	{
+
+	}
+
+}
