6d6a5a34203871ee3ef30b45bb66ae1dbc3a1994
Pablo Galve <pablogalve100@gmail.com>
2021-05-14 16:30:03 +0200

    initial video to homepage + add initial template

---
+++ web_content.cpp
@@ -0,0 +1,27 @@
+/* HOMEPAGE
+    - Impressive Video/Gif of 15 seconds
+*/
+
+/* GAME
+    - 9 gifs
+    - Trailer
+*/
+
+/* ENGINE
+    - Brief description of each system
+    - 3 gifs
+*/
+
+/* TEAM
+    // Picture of each member and their role
+    // A dedicated webpage for each member
+    - Social Media (Github, Linkedin, Portfolio)
+    - Description
+    - Contribution
+    - 3-6 gifs
+*/
+
+/* DOWNLOAD
+    - First iteration -> Link to github releases
+    - Second iteration -> Direct download
+*/
