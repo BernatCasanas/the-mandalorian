982e59e99403bdc3541f6d43a10d81f3f0c7114d
Ferran-Roger Basart i Bosch <47557484+ferba93@users.noreply.github.com>
2021-05-20 19:38:38 +0200

    [Wampa & Skel] Attack's camera shakes added

--- Diamond Engine/Project Folder/Assets/Scripts/AtackBosslv2.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/AtackBosslv2.cs
@@ -4,6 +4,8 @@ using DiamondEngine;
 public class AtackBosslv2 : DiamondComponent
 {
     public float damage = 0.0f;
+    public float rumbleTime = 0.5f;
+    public GameObject camera = null;
 
     public void OnTriggerEnter(GameObject triggeredGameObject)
     {
@@ -14,6 +16,17 @@ public class AtackBosslv2 : DiamondComponent
             {
                 health.TakeDamage((int)damage);
                 gameObject.GetComponent<AtackBosslv2>().active = false;
+            }
+
+            if (camera != null)
+            {
+                Shake3D shake = camera.GetComponent<Shake3D>();
+                if (shake != null)
+                {
+                    shake.StartShaking(0.8f, 0.1f);
+                }
+
+                Input.PlayHaptic(0.9f, (int)rumbleTime * 1000);
             }
         }
     }
