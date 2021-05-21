74706d37dd3da91e387994c761298be78b828e29
Ap011y0n <47557365+Ap011y0n@users.noreply.github.com>
2021-05-21 17:17:24 +0200

    Update GameResources.cs

--- Diamond Engine/Project Folder/Assets/Scripts/GameResources.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/GameResources.cs
@@ -653,34 +653,34 @@ static class BoonDataHolder
 {
     static BoonDataHolder()
     {
-        boonType[(int)BOONS.BOON_ANAKIN_KILL_STREAK] = new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_BOKATAN_RESILENCE] =          new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_BOSSK_STRENGTH] =             new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_BOUNTY_HUNTER_SKILLS] =       new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_CADBANE_ROCKET_BOOTS] =       new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_CADBANE_SLEIGHT] =            new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_ECHO_QUICK_RECOVERY] =        new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_GREEF_PAYCHECK] =             new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_ITS_A_TRAP] =                 new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_MANDALORIAN_QUICK_DRAW] =     new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_REX_SECOND_BLASTER] =         new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_SOLO_QUICK_DRAW] =            new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_WATTOS_COOLANT] =             new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_WRECKER_HEAVY_SHOT] =         new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_WRECKER_RESILIENCE] =         new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_BLAST_CANNON_MOUTHPIECE] =    new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_GREEDO_QUICKSHOOTER] =        new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_MANDALORIAN_QUICK_COMBO] =    new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_FENNEC_SNIPER_RIFLE] =        new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_BOBBA_FETT_STUN_AMM] =        new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_BOSSK_SPECIAL_AMMO] =         new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_GEOTERMAL_MARKER] =           new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_MASTER_LUMINARA_FORCE] =      new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_MASTER_WINDU_FORCE] =         new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_MASTER_YODA_FORCE] =          new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_MANDALORIAN_CODE] =           new SoloQuickDraw();
-        boonType[(int)BOONS.CROSSHAIR_LUCKY_SHOT] =            new SoloQuickDraw();
-        boonType[(int)BOONS.BOON_AHSOKA_DETERMINATION] =       new SoloQuickDraw();
+        boonType[(int)BOONS.BOON_ANAKIN_KILL_STREAK] =         new AnakinKillstreak();
+        boonType[(int)BOONS.BOON_BOKATAN_RESILENCE] =          new BokatanResilence();
+        boonType[(int)BOONS.BOON_BOSSK_STRENGTH] =             new BosskStrength();
+        boonType[(int)BOONS.BOON_BOUNTY_HUNTER_SKILLS] =       new BountyHunter();
+        boonType[(int)BOONS.BOON_CADBANE_ROCKET_BOOTS] =       new CadBaneBoots();
+        boonType[(int)BOONS.BOON_CADBANE_SLEIGHT] =            new CadBaneSoH();
+        boonType[(int)BOONS.BOON_ECHO_QUICK_RECOVERY] =        new EchoRecovery();
+        boonType[(int)BOONS.BOON_GREEF_PAYCHECK] =             new EchoRecovery();     //Add this when done = new GreefPaycheck();
+        boonType[(int)BOONS.BOON_ITS_A_TRAP] =                 new ItsATrap();
+        boonType[(int)BOONS.BOON_MANDALORIAN_QUICK_DRAW] =     new MandoQuickDraw();
+        boonType[(int)BOONS.BOON_REX_SECOND_BLASTER] =         new RexSecBlaster();
+        boonType[(int)BOONS.BOON_SOLO_QUICK_DRAW] =            new SoloQuickDraw();     
+        boonType[(int)BOONS.BOON_WATTOS_COOLANT] =             new WattoCoolant();
+        boonType[(int)BOONS.BOON_WRECKER_HEAVY_SHOT] =         new WreckHeavyShot();
+        boonType[(int)BOONS.BOON_WRECKER_RESILIENCE] =         new WreckerResilence();
+        boonType[(int)BOONS.BOON_BLAST_CANNON_MOUTHPIECE] =    new BlastCannonMouthpiece();
+        boonType[(int)BOONS.BOON_GREEDO_QUICKSHOOTER] =        new GreedoShooter();
+        boonType[(int)BOONS.BOON_MANDALORIAN_QUICK_COMBO] =    new MandoQuickCombo();
+        boonType[(int)BOONS.BOON_FENNEC_SNIPER_RIFLE] =        new FennecSniper();
+        boonType[(int)BOONS.BOON_BOBBA_FETT_STUN_AMM] =        new BobbaStunAmmo();    
+        boonType[(int)BOONS.BOON_BOSSK_SPECIAL_AMMO] =         new BosskSpecialAmmo();
+        boonType[(int)BOONS.BOON_GEOTERMAL_MARKER] =           new BosskSpecialAmmo(); //Add this when done= new GeotermalMarker();
+        boonType[(int)BOONS.BOON_MASTER_LUMINARA_FORCE] =      new LuminaraForce();
+        boonType[(int)BOONS.BOON_MASTER_WINDU_FORCE] =         new WinduForceControl();
+        boonType[(int)BOONS.BOON_MASTER_YODA_FORCE] =          new YodaForceControl();
+        boonType[(int)BOONS.BOON_MANDALORIAN_CODE] =           new MandoCode();
+        boonType[(int)BOONS.CROSSHAIR_LUCKY_SHOT] =            new CossHairLuckyShot();
+        boonType[(int)BOONS.BOON_AHSOKA_DETERMINATION] =       new AhsokaDetermination();
 
         for (int i = 0; i < boonType.Length; i++)
         {
