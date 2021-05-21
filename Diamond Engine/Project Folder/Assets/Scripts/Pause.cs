863372f4e38b14f52df4191850f1a8424c2369ca
Marc Pagès Francesch <47534017+marcpages2020@users.noreply.github.com>
2021-05-21 20:56:56 +0200

    [CODE] Adding current boons to AllPause

--- Diamond Engine/Project Folder/Assets/Scripts/Pause.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/Pause.cs
@@ -10,14 +10,7 @@ public class Pause : DiamondComponent
 	public GameObject quitWindow = null;
 	public GameObject background = null;
 	public GameObject default_selected = null;
-
-	public GameObject leftImage = null;
-	public GameObject rightImage = null;
-	public GameObject leftText = null;
-	public GameObject rightText = null;
-	public GameObject leftX = null;
-	public GameObject rightX = null;
-
+	public GameObject boonDisplayObject = null;
 	public GameObject shop = null;
 
 	public void OnExecuteButton()
@@ -83,82 +76,19 @@ public class Pause : DiamondComponent
 					componentNavigation.Select();
             }
 
-			leftImage.Enable(true);
-			leftText.Enable(true);
-			leftX.Enable(true);
-			rightImage.Enable(true);
-			rightText.Enable(true);
-			rightX.Enable(true);
+			if (boonDisplayObject != null)
+			{
+				BoonDisplay boonDisplay = boonDisplayObject.GetComponent<BoonDisplay>();
 
-			//All this must be redone
-
-			//if (Counter.GameCounters.ContainsKey(Counter.CounterTypes.BOKATAN_RES) || Counter.GameCounters.ContainsKey(Counter.CounterTypes.WRECKER_RES))
-			//{
-			//	if (bo * boPlace < wr * wrPlace)
-			//	{
-
-			//		if (Counter.GameCounters.ContainsKey(Counter.CounterTypes.BOKATAN_RES))
-			//		{
-			//			rightText.GetComponent<Text>().text = Counter.GameCounters[Counter.CounterTypes.BOKATAN_RES].amount.ToString();
-			//			Debug.Log(Counter.GameCounters[Counter.CounterTypes.BOKATAN_RES].amount.ToString());
-			//		}
-			//		else
-			//		{
-			//			rightImage.Enable(false);
-			//			rightText.Enable(false);
-			//			rightX.Enable(false);
-			//		}
-
-			//		if (Counter.GameCounters.ContainsKey(Counter.CounterTypes.WRECKER_RES))
-			//		{
-			//			leftText.GetComponent<Text>().text = Counter.GameCounters[Counter.CounterTypes.WRECKER_RES].amount.ToString();
-			//			Debug.Log(Counter.GameCounters[Counter.CounterTypes.WRECKER_RES].amount.ToString());
-			//		}
-			//		else
-			//		{
-			//			leftImage.Enable(false);
-			//			leftText.Enable(false);
-			//			leftX.Enable(false);
-			//		}
-			//	}
-
-			//	else
-			//	{
-			//		rightImage.GetComponent<Image2D>().SwapTwoImages(leftImage);
-			//		if (Counter.GameCounters.ContainsKey(Counter.CounterTypes.BOKATAN_RES))
-			//		{
-			//			leftText.GetComponent<Text>().text = Counter.GameCounters[Counter.CounterTypes.BOKATAN_RES].amount.ToString();
-			//			Debug.Log(Counter.GameCounters[Counter.CounterTypes.BOKATAN_RES].amount.ToString());
-			//		}
-			//		else
-			//		{
-			//			leftImage.Enable(false);
-			//			leftText.Enable(false);
-			//			leftX.Enable(false);
-			//		}
-
-			//		if (Counter.GameCounters.ContainsKey(Counter.CounterTypes.WRECKER_RES))
-			//		{
-			//			rightText.GetComponent<Text>().text = Counter.GameCounters[Counter.CounterTypes.WRECKER_RES].amount.ToString();
-			//			Debug.Log(Counter.GameCounters[Counter.CounterTypes.WRECKER_RES].amount.ToString());
-			//		}
-			//		else
-			//		{
-			//			rightImage.Enable(false);
-			//			rightText.Enable(false);
-			//			rightX.Enable(false);
-			//		}
-			//	}
-			//}
-			//else
-			//{
-			//	leftImage.Enable(false);
-			//	leftText.Enable(false);
-			//	leftX.Enable(false);
-			//	rightImage.Enable(false);
-			//	rightText.Enable(false);
-			//	rightX.Enable(false);
-			//}
+				if (boonDisplay != null)
+				{
+					boonDisplay.ShowBoons();
+				}
+				else
+				{
+					Debug.Log("Null boon display");
+				}
+			}
 		}
 	}
 
