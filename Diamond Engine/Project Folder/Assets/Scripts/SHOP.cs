d67e688ff50282228e9d4312eca967c6e9ce9687
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2021-05-23 13:43:07 +0200

    [SFX] Enironment SFX changes

--- Diamond Engine/Project Folder/Assets/Scripts/SHOP.cs
+++ Diamond Engine/Project Folder/Assets/Scripts/SHOP.cs
@@ -38,7 +38,7 @@ public class SHOP : DiamondComponent
     public bool autoGenerateItems = true;
     public bool opening;
     public GameObject defaultButton = null;
-
+    private GameObject envObj = null;
     bool shopOpen = false;
     private bool start = true;
 
@@ -62,6 +62,7 @@ public class SHOP : DiamondComponent
             if (items[i] != null)
                 shopButtons[i] = items[i].GetComponent<ShopButtons>();
         }
+        envObj = InternalCalls.FindObjectWithName("Environment");
     }
 
     private void Start()
@@ -76,6 +77,7 @@ public class SHOP : DiamondComponent
             Audio.SetState("Player_State", "Alive");
             if (MusicSourceLocate.instance != null)
                 Audio.SetSwitch(MusicSourceLocate.instance.gameObject, "Player_Action", "Exploring");
+            Audio.PlayAudio(envObj, "Play_Post_Boss_Room_1_Ambience");
         }
         else if (RoomSwitch.currentLevelIndicator-1 == RoomSwitch.LEVELS.TWO)
         {
@@ -83,6 +85,7 @@ public class SHOP : DiamondComponent
             Audio.SetState("Player_State", "Alive");
             if (MusicSourceLocate.instance != null)
                 Audio.SetSwitch(MusicSourceLocate.instance.gameObject, "Player_Action", "Exploring");
+            Audio.PlayAudio(envObj, "Play_Post_Boss_Room_2_Ambience");
         }
         else if (RoomSwitch.currentLevelIndicator == RoomSwitch.LEVELS.THREE)
         {
