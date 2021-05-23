97f3dba53306a209a777994ca733f5ad52c88b81
Jordi Pardo <jordipardogutierrez@gmail.com>
2021-05-23 22:23:22 +0200

    [CODE] Added DPSDamage script

---
+++ Diamond Engine/Project Folder/Assets/Scripts/DPSDamage.cs
@@ -0,0 +1,80 @@
+using System;
+using DiamondEngine;
+
+public class DPSDamage : DiamondComponent
+{
+    public bool onTriggerStay = false;
+    public int base_damage = 1;
+    public int damage = 0;
+    public float ticksPerSecond = 1f;
+
+    public float damageTimer = 0.0f;
+
+
+
+    //public Func<int,bool,int> onDealDamage;
+
+
+    public void Awake()
+    {
+        damage = base_damage;
+        damageTimer = ticksPerSecond;
+        //if (Core.instance != null)
+        //{
+        //    onDealDamage = Core.instance.gameObject.GetComponent<PlayerHealth>().TakeDamage;
+        //}
+    }
+
+    public void Update()
+    {
+        if (!onTriggerStay)
+        {
+
+            return;
+        }
+
+        if (damageTimer < ticksPerSecond)
+        {
+            damageTimer += Time.deltaTime;
+        }
+        else
+        {
+            damageTimer = 0.0f;
+            //onDealDamage?.Invoke(damage,false);
+            Core.instance.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage, true);
+            IncrementDamage();
+            Debug.Log("Water Damage");
+        }
+    }
+
+
+    public void OnTriggerEnter(GameObject other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onTriggerStay = true;
+        }
+    }
+
+    public void OnTriggerExit(GameObject other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onTriggerStay = false;
+            damageTimer = 0;
+            damage = base_damage;
+        }
+    }
+
+    private void IncrementDamage()
+    {
+        if (damage < 10)
+        {
+            damage *= 2;
+            if (damage > 10)
+            {
+                damage = 10;
+            }
+        }
+    }
+}
